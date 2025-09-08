cd3f0b5c258c03db2fb339b0ae8b1f03e8e5f997
Rasec Romero <rasec.romero46@gmail.com>
2025-09-08 17:41:22 -0600

    v4

--- app/src/main/java/com/example/foodapp/Components/Products.kt
+++ app/src/main/java/com/example/foodapp/Components/Products.kt
@@ -1,14 +1,19 @@
 package com.example.foodapp.Components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
@@ -16,12 +21,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.example.foodapp.data.ProductItem
+import com.example.foodapp.ui.theme.BackgroundColor
 import com.example.foodapp.ui.theme.DarkColor
+import com.example.foodapp.ui.theme.LightColor
+import com.example.foodapp.ui.theme.PrimaryColor
+import kotlinx.coroutines.sync.Mutex
 
 @Composable
 fun ProductGrid(products: List<ProductItem>) {
@@ -42,34 +53,48 @@ fun ProductsCard(product: ProductItem) {
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
     Box(
-
+      modifier = Modifier.size(150.dp)
     ) {
       AsyncImage(
         model = product.image,
         contentDescription = product.name,
-        modifier = Modifier.size(150.dp)
+        modifier = Modifier
+          .fillMaxSize()
       )
       Text(
-        text = "$" + product.price.toString()
+        text = "$" + product.price.toString(),
+        color = BackgroundColor,
+        modifier = Modifier
+          .align(Alignment.BottomEnd)
+          .clip(RoundedCornerShape(8.dp))
+          .background(PrimaryColor)
+          .padding(4.dp)
       )
     }
     Row(
-
+      horizontalArrangement = Arrangement.Center,
+      verticalAlignment = Alignment.CenterVertically
     ) {
       Icon(
         imageVector = Icons.Default.Star,
         contentDescription = "Stars",
-        modifier = Modifier.size(10.dp),
-        tint = DarkColor
+        modifier = Modifier.size(14.dp),
+        tint = LightColor
       )
       Text(
         text = product.rating.toString(),
-        fontSize = 10.sp
+        fontSize = 10.sp,
+        color = PrimaryColor,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(end = 4.dp, top = 2.dp)
       )
       Text(
         text = product.name,
-        fontSize = 10.sp
+        color = PrimaryColor,
+        fontSize = 10.sp,
+        modifier = Modifier.padding(top = 2.dp)
       )
+
     }
   }
 }
